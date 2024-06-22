8c2e7dc103fac4dd57e56aed4656871cef6e90b0
crislzno2001 <crislzno2001@gmail.com>
2024-06-23 01:22:45 +0200

    hola

--- Assets/Scripts/Grupo12/MyTester.cs
+++ Assets/Scripts/Grupo12/MyTester.cs
@@ -1,72 +1,75 @@
 using NavigationDJIA.World;
 using QMind.Interfaces;
 using System;
-using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using UnityEngine;
 
 public class MyTester : IQMind
 {
-    private WorldInfo _worldInfo;
-    private QTable _QTable;
+    private WorldInfo _worldInfo; // Información sobre el mundo en el que se mueve el agente
+    private QTable _qTable; // La tabla Q que contiene los valores Q entrenados
 
+    // Inicializa el tester con la información del mundo y carga la tabla Q desde un archivo
     public void Initialize(WorldInfo worldInfo)
     {
-        _worldInfo = worldInfo;
-        _QTable = new QTable(worldInfo); // Se inicializa la QTable con el mundo
-        _QTable.InicializarTabla();
-        LoadQTable();
+        _worldInfo = worldInfo; // Asigna la información del mundo
+        _qTable = new QTable(worldInfo); // Inicializa la QTable con el mundo
+        _qTable.InitializeTable(); // Inicializa la tabla Q con valores predeterminados
+        LoadQTable(); // Carga los valores Q desde un archivo
     }
 
+    // Calcula el siguiente paso del agente basado en la posición actual y la del otro jugador
     public CellInfo GetNextStep(CellInfo currentPosition, CellInfo otherPosition)
     {
-        QState state = CalculateState(currentPosition, otherPosition);
-        CellInfo agentCell = null;
+        QState state = CalculateState(currentPosition, otherPosition); // Calcula el estado actual
+        int action = GetAction(state); // Obtiene la mejor acción basada en la tabla Q
+        CellInfo agentCell = QMind.Utils.MoveAgent(action, currentPosition, _worldInfo); // Mueve el agente según la acción seleccionada
+        Debug.Log("Action = " + action); // Imprime la acción para depuración
 
-        int action = GetAction(state);
-        agentCell = QMind.Utils.MoveAgent(action, currentPosition, _worldInfo);
-        Debug.Log("Action = " + action);
-
+        // Si la celda siguiente no es transitable, recalcula el estado (esto puede necesitar una mejor estrategia de manejo)
         if (!agentCell.Walkable)
         {
-            CalculateState(currentPosition, otherPosition);
+            state = CalculateState(currentPosition, otherPosition); // Recalcula el estado
         }
 
-        Debug.Log(currentPosition.x.ToString() + "" + currentPosition.y.ToString());
+        Debug.Log(currentPosition.x.ToString() + "" + currentPosition.y.ToString()); // Imprime la posición actual para depuración
 
-        return agentCell;
+        return agentCell; // Devuelve la celda siguiente del agente
     }
 
+    // Obtiene la mejor acción para un estado dado usando la tabla Q
     private int GetAction(QState state)
     {
-        return _QTable.DevolverMejorAccion(state._nWalkable, state._sWalkable, state._eWalkable, state._wWalkable, state._playerUp, state._playerRight);
+        return _qTable.GetBestAction(state._nWalkable, state._sWalkable, state._eWalkable, state._wWalkable, state._playerUp, state._playerRight);
     }
 
+    // Carga los valores Q desde un archivo CSV en la tabla Q
     private void LoadQTable()
     {
-        string filePath = @"Assets/Scripts/Grupo12/TablaQ.csv";
-        if (File.Exists(filePath))
+        string filePath = @"Assets/Scripts/Grupo12/TablaQ.csv"; // Ruta del archivo CSV que contiene la tabla Q
+        if (File.Exists(filePath)) // Verifica si el archivo existe
         {
-            using (StreamReader reader = new StreamReader(File.OpenRead(filePath)))
+            using (StreamReader reader = new StreamReader(File.OpenRead(filePath))) // Abre el archivo para lectura
             {
-                int rowCount = 0;
-                while (!reader.EndOfStream && rowCount < _QTable._numRows)
+                int rowCount = 0; // Contador de filas
+                while (!reader.EndOfStream && rowCount < _qTable.ActionsCount) // Lee hasta el final del archivo o hasta llenar la tabla Q
                 {
-                    var line = reader.ReadLine();
-                    var values = line.Split(';');
-                    for (int col = 0; col < values.Length; col++)
+                    var line = reader.ReadLine(); // Lee una línea del archivo
+                    var values = line.Split(';'); // Separa los valores por el delimitador ';'
+                    for (int col = 0; col < values.Length; col++) // Itera sobre las columnas
                     {
-                        _QTable._tablaQ[rowCount, col] = float.Parse(values[col]);
+                        _qTable.QTableValues[rowCount, col] = float.Parse(values[col]); // Asigna el valor Q leído a la tabla Q
                     }
-                    rowCount++;
+                    rowCount++; // Incrementa el contador de filas
                 }
             }
         }
     }
 
+    // Calcula el estado basado en la posición actual del agente y la del otro jugador
     private QState CalculateState(CellInfo currentPosition, CellInfo otherPosition)
     {
-        return new QState(currentPosition, otherPosition, _worldInfo);
+        return new QState(currentPosition, otherPosition, _worldInfo); // Crea un nuevo estado QState basado en las posiciones actuales
     }
 }
--- Assets/Scripts/Grupo12/QTable.cs
+++ Assets/Scripts/Grupo12/QTable.cs
@@ -1,134 +1,131 @@
 // QTable gestiona la tabla Q, que guarda los valores Q usados para decidir las acciones del agente.
 
 using NavigationDJIA.World;
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class QTable : MonoBehaviour
 {
-    public int _numRows; // Número de acciones posibles
-    public int _numCols; // Número de estados posibles
-    public float[,] _tablaQ; // Tabla que almacena los valores Q
-    public QState[] _listaEstados; // Lista de todos los estados posibles
+    private const int NumActions = 4; // Número de acciones posibles
+    private const int NumStates = 16 * 9; // Número de estados posibles
+    private readonly float[,] _qTable; // Tabla que almacena los valores Q
+    private readonly QState[] _stateList; // Lista de todos los estados posibles
 
     // Constructor que inicializa la tabla Q con el mundo dado
     public QTable(WorldInfo world)
     {
-        this._numRows = 4; // Cuatro acciones: mover norte, sur, este, oeste
-        this._numCols = 16 * 9; // Número de estados posibles
-        this._tablaQ = new float[this._numRows, this._numCols]; // Inicializa la tabla Q
-        this._listaEstados = new QState[_numCols]; // Inicializa la lista de estados
-        InicializarTabla(); // Llena la tabla Q con valores iniciales
+        _qTable = new float[NumActions, NumStates]; // Inicializa la tabla Q
+        _stateList = new QState[NumStates]; // Inicializa la lista de estados
+        InitializeTable(); // Llena la tabla Q con valores iniciales
     }
 
-    // Inicializa la tabla Q con valores predeterminados
-    public void InicializarTabla()
-    {
-        for (int i = 0; i < _numRows; i++)
-        {
-            for (int j = 0; j < _numCols; j++)
-            {
-                _tablaQ[i, j] = 0; // Inicializa todos los valores Q a 0
-            }
-        }
+    // Propiedad para acceder al número de acciones
+    public int ActionsCount => NumActions;
 
-        InicializarEstados(); // Crea todos los estados posibles
+    // Propiedad para acceder a la tabla Q
+    public float[,] QTableValues => _qTable;
+
+    // Inicializa la tabla Q con valores predeterminados
+    public void InitializeTable()
+    {
+        // Inicializa todos los valores Q a 0
+        System.Array.Clear(_qTable, 0, _qTable.Length);
+        InitializeStates(); // Crea todos los estados posibles
     }
 
     // Inicializa los estados posibles en la tabla Q
-    public void InicializarEstados()
+    private void InitializeStates()
     {
-        int indice = 0;
-        bool n, s, e, w;
+        int index = 0;
         for (int i = 0; i < 16; i++)
         {
+            bool n = (i & 8) != 0; // i / 8 % 2 != 0
+            bool s = (i & 4) != 0; // i / 4 % 2 != 0
+            bool e = (i & 2) != 0; // i / 2 % 2 != 0
+            bool w = (i & 1) != 0; // i % 2 != 0
+
             for (int j = 0; j < 3; j++)
             {
                 for (int k = 0; k < 3; k++)
                 {
-                    // Determina si las direcciones son transitables
-                    n = (i / 8 % 2 != 0);
-                    s = (i / 4 % 2 != 0);
-                    e = (i / 2 % 2 != 0);
-                    w = (i % 2 != 0);
-
-                    _listaEstados[indice] = new QState(n, s, e, w, j, k, indice); // Crea un nuevo estado
-                    indice++;
+                    _stateList[index] = new QState(n, s, e, w, j, k, index); // Crea un nuevo estado
+                    index++;
                 }
             }
         }
     }
 
     // Devuelve el valor Q para una acción específica
-    public float DevolverQ(int accion, bool n, bool s, bool e, bool w, int up, int right)
+    public float GetQValue(int action, bool n, bool s, bool e, bool w, int up, int right)
     {
-        int indice = BuscarEstado(n, s, e, w, up, right); // Busca el estado correspondiente
-        return _tablaQ[accion, indice]; // Devuelve el valor Q
+        int index = FindStateIndex(n, s, e, w, up, right); // Busca el estado correspondiente
+        return _qTable[action, index]; // Devuelve el valor Q
     }
 
     // Devuelve la mejor acción basada en el estado actual
-    public int DevolverMejorAccion(bool n, bool s, bool e, bool w, int up, int right)
+    public int GetBestAction(bool n, bool s, bool e, bool w, int up, int right)
     {
-        int indice = BuscarEstado(n, s, e, w, up, right); // Busca el estado correspondiente
+        int index = FindStateIndex(n, s, e, w, up, right); // Busca el estado correspondiente
 
-        int mejorAccion = 0;
-        float mejorQ = _tablaQ[0, indice]; // Inicializa con el primer valor Q
+        int bestAction = 0;
+        float bestQValue = _qTable[0, index]; // Inicializa con el primer valor Q
 
         // Compara para encontrar el mejor valor Q y su acción
-        for (int i = 1; i < _numRows; i++)
+        for (int i = 1; i < NumActions; i++)
         {
-            if (_tablaQ[i, indice] > mejorQ)
+            if (_qTable[i, index] > bestQValue)
             {
-                mejorAccion = i;
-                mejorQ = _tablaQ[i, indice];
+                bestAction = i;
+                bestQValue = _qTable[i, index];
             }
         }
 
-        return mejorAccion; // Devuelve la mejor acción
+        return bestAction; // Devuelve la mejor acción
     }
 
     // Devuelve el mejor valor Q basado en el estado actual
-    public float DevolverMejorQ(bool n, bool s, bool e, bool w, int up, int right)
+    public float GetBestQValue(bool n, bool s, bool e, bool w, int up, int right)
     {
-        int indice = BuscarEstado(n, s, e, w, up, right); // Busca el estado correspondiente
+        int index = FindStateIndex(n, s, e, w, up, right); // Busca el estado correspondiente
 
-        float mejorQ = _tablaQ[0, indice]; // Inicializa con el primer valor Q
+        float bestQValue = _qTable[0, index]; // Inicializa con el primer valor Q
 
         // Compara para encontrar el mejor valor Q
-        for (int i = 1; i < _numRows; i++)
+        for (int i = 1; i < NumActions; i++)
         {
-            if (_tablaQ[i, indice] > mejorQ)
+            if (_qTable[i, index] > bestQValue)
             {
-                mejorQ = _tablaQ[i, indice];
+                bestQValue = _qTable[i, index];
             }
         }
 
-        return mejorQ; // Devuelve el mejor valor Q
+        return bestQValue; // Devuelve el mejor valor Q
     }
 
     // Actualiza el valor Q para una acción específica
-    public void ActualizarQ(int accion, bool n, bool s, bool e, bool w, int up, int right, float actualizedQ)
+    public void UpdateQ(int action, bool n, bool s, bool e, bool w, int up, int right, float updatedQ)
     {
-        int indice = BuscarEstado(n, s, e, w, up, right); // Busca el estado correspondiente
-        _tablaQ[accion, indice] = actualizedQ; // Actualiza el valor Q
+        int index = FindStateIndex(n, s, e, w, up, right); // Busca el estado correspondiente
+        _qTable[action, index] = updatedQ; // Actualiza el valor Q
     }
 
     // Busca el estado correspondiente en la lista de estados
-    private int BuscarEstado(bool n, bool s, bool e, bool w, int up, int right)
+    private int FindStateIndex(bool n, bool s, bool e, bool w, int up, int right)
     {
-        for (int i = 0; i < _listaEstados.Length; i++)
+        for (int i = 0; i < _stateList.Length; i++)
         {
-            if (_listaEstados[i]._nWalkable == n &&
-                _listaEstados[i]._sWalkable == s &&
-                _listaEstados[i]._eWalkable == e &&
-                _listaEstados[i]._wWalkable == w &&
-                _listaEstados[i]._playerUp == up &&
-                _listaEstados[i]._playerRight == right)
+            QState state = _stateList[i];
+            if (state._nWalkable == n &&
+                state._sWalkable == s &&
+                state._eWalkable == e &&
+                state._wWalkable == w &&
+                state._playerUp == up &&
+                state._playerRight == right)
             {
-                return _listaEstados[i]._idState; // Devuelve el ID del estado encontrado
+                return state._idState; // Devuelve el ID del estado encontrado
             }
         }
+        Debug.LogError("Estado no encontrado"); // Registra un error si el estado no se encuentra
         return -1; // Estado no encontrado
     }
 }
